17c6c1688d6fa17786db2ebcb824dc6cb5ea248c
mrm <kei.id0305@gmail.com>
2021-01-09 23:32:52 +0900

    ログインできる

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    user = User.find_by(username: params[:session][:username])
+
+    if user && user.authenticate(params[:session][:password]) then
+      session[:user_id] = user.id
+      redirect_to root_url
+    end
+  end
 end
