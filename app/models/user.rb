cab49ff43c06b604e34d394fe5818633f6aafff8
mrm <kei.id0305@gmail.com>
2021-01-09 20:53:36 +0900

    ヴァリでーしょん

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :posts
+
+  validates :screenname, length: { in: 1..100 }
+  validates :username, length: { in: 1..20 }, format: { with: /\A[a-zA-z0-9_]+\z/ }
+  validates :profile, length: { in: 1..100 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
-  delete 'login' => 'sessions#delete'
+  delete 'login' => 'sessions#destroy'
   resources :posts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
