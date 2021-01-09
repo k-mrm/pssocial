969630539841af0392723501755a8fe9061b5c23
mrm <kei.id0305@gmail.com>
2021-01-09 21:05:42 +0900

    rooooot

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts
 
+  # see: https://railsguides.jp/active_record_validations.html
   validates :screenname, length: { in: 1..100 }
   validates :username, length: { in: 1..20 }, format: { with: /\A[a-zA-z0-9_]+\z/ }
   validates :profile, length: { in: 1..100 }
