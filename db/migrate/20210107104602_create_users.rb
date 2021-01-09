95e0391f8e74fd708f8b2f59f0c0567cc4ef69f8
mrm <kei.id0305@gmail.com>
2021-01-09 20:26:59 +0900

    メェ

--- db/migrate/20210107104602_create_users.rb
+++ db/migrate/20210107104602_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :screenname
-      t.string :username
-      t.string :email
-      t.string :password_digest
-      t.text :profile
+      t.string :screenname, null: false
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.boolean :admin, null: false
+      t.text :profile, null: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,22 @@ ActiveRecord::Schema.define(version: 2021_01_07_105350) do
   create_table "posts", force: :cascade do |t|
     t.integer "postid"
     t.text "content"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "screenname"
-    t.string "username"
-    t.integer "userid"
+    t.string "screenname", null: false
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.boolean "admin", null: false
+    t.text "profile"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
